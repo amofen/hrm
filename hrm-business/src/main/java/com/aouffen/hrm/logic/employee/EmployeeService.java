242a7080e2cb4c64f27437f9279c9d332f3b5da9
Amine Aouffen <amineaouffen@hotmail.com>
2018-12-08 14:09:11 +0100

    #archInit : initiation of maven pom

---
+++ hrm-business/src/main/java/com/aouffen/hrm/logic/employee/EmployeeService.java
@@ -0,0 +1,14 @@
+package com.aouffen.hrm.logic.employee;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateUtils;
+
+import com.aouffen.hrm.model.employee.Employee;
+
+public class EmployeeService {
+	public int calculateEmployeeAge(Employee employee) {
+		return 0;
+	}
+
+}
---
+++ hrm-business/src/test/java/com/aouffen/hrm/BusinessTest.java
@@ -0,0 +1,5 @@
+package com.aouffen.hrm;
+
+public class BusinessTest {
+
+}
